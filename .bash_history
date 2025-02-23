3e1fd500caa78bedd0134386c9b56c316b671a83
JerushaStewart <jerushastewa2022@fau.edu>
2025-02-23 01:31:03 +0000

    2/22/2025

---
+++ .bash_history
@@ -0,0 +1,49 @@
+gcloud run services list
+gcloud services enable sourcerepo.googleapis.com
+git config --global http.cookiefile "%USERPROFILE%\.gitcookies"
+powershell -noprofile -nologo -command Write-Output "source.developers.google.com`tFALSE`t/`tTRUE`t2147483647`to`tgit-jerushastewa2022.fau.edu=1//01eCuIovmH7xwCgYIARAAGAESNwF-L9Ir7ivQI2rRrqUba11RKVwN_uRrWWKLL5P2HBtuo7uaXeouhwMqvwWnAVCXprLW9hUNCCk" >>"%USERPROFILE%\.gitcookies"
+cloudshell_open --repo_url "https://source.developers.google.com/p/project-1-449321/r/github_jerushastewart_googleproject1" --page "editor" --open_in_editor "./" --git_branch "main" --force_new_clone
+git config --global user.name "JerushaStewart"
+git config --global user.email "jerushastewa2022@fau.edu"
+git branch --set-upstream-to=GoogleProject1/<branch> main
+git branch --set-upstream-to=origin/<branch> main
+git branch --set-upstream-to=origin/main main
+git push -u origin main
+git push -u GoogleProject1 main
+git push -u GoogleProject1 main 
+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
+git push -u GoogleProject1 main
+git pull
+git push -u GoogleProject1 main
+git pull -u GoogleProject1 main
+git pull GoogleProject1 main
+git config pull.rebase false
+git pull GoogleProject1 main
+git config pull.rebase false
+git pull GoogleProject1 main
+git config --global pull.rebase true
+git pull GoogleProject1 main
+git pull GoogleProject1 main --allow-unrelated-histories
+git push -u GoogleProject1 main
+/bin/python /home/jerushastewa2022/main.py
+git pull
+git push
+git push
+git pull
+git commit -m "2/22/2025"
+git commit -a
+git commit -m
+git commit -m "changed"
+git commit -m "2/22/2025"
+git push origin master
+git add
+git commit
+cd /home/jerushastewa2022/project-1-449321
+git commit -m "foof"
+git add
+cd repository
+git status
+git add
+git commit -a
+rm -f .git/index.lock
+git add
---
+++ .codeoss/data/User/History/-4d9dbedd/Hb0c.py
@@ -0,0 +1,234 @@
+import time
+import os
+from flask import Flask, redirect, request, send_file
+
+os.makedirs('files', exist_ok = True)
+app = Flask(__name__)
+
+
+
+@app.route('/')
+def index():
+    index_html="""
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Image insert</title>
+ <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Style+Script&display=swap" rel="stylesheet">
+    <link href="/style.css" rel="stylesheet" type="text/css" media="all">
+    
+  </head>
+  
+  
+  <body>
+  
+  <center><h1 class="title">To Upload or Not to Upload</h1></center>
+  
+  <div class="main_box">
+    <h1>Upload Images Below</h1>
+     <form method="post" enctype="multipart/form-data" action="/upload" >
+  <div>
+    <label for="file">Choose file to upload</label>
+    <input type="file" id="file" name="form_file" accept="image/jpeg"/>
+  </div>
+  <div>
+    <button>Submit</button>
+  </div>
+</form>
+"""
+end_html="""
+    </div>
+ <style>
+ * {
+   margin: 0;
+   padding:0;
+   
+   }
+   
+   body {
+     background-color: white;
+     }
+   
+   .title {
+     font-family: "Style Script", serif;
+     font-weight: 400;
+     font-style: bold;
+     font-size: 50px;
+     color: #ff62bf;
+     }
+   
+  .main_box {
+    border-radius: 5px;
+    border: 5px solid #ff62bf;
+    background-color: pink;
+    margin: 10px 200px 0 200px;
+    padding:25px;
+    
+    }
+    
+ h1 {
+   font-size: 10px;
+   font-weight: bold;
+   }
+ 
+ .box {
+   border: 1px solid black;
+   }
+
+   a{
+    text-decoration: none; 
+   }
+
+ </style> 
+ </body>
+  
+</html>
+"""    
+
+    for file in list_files():
+        index_html += "<li><a href=\"/files/" + file + "\">" + file + "</a></li>" + end
+
+    return index_html
+
+@app.route('/upload', methods=["POST"])
+def upload():
+    file = request.files['form_file']  # item name must match name in HTML form
+    file.save(os.path.join("./files", file.filename))
+
+    return redirect("/")
+
+@app.route('/files')
+def list_files():
+    files = os.listdir("./files")
+    jpegs = []
+    for file in files:
+        if file.lower().endswith(".jpeg") or file.lower().endswith(".jpg"):
+            jpegs.append(file)
+    
+    return jpegs
+
+@app.route('/files/<filename>')
+def get_file(filename):
+  return send_file('./files/'+filename)
+
+
+if __name__ == "__main__":
+    # Development only: run "python main.py" and open http://localhost:8080
+    # When deploying to Cloud Run, a production-grade WSGI HTTP server,
+    # such as Gunicorn, will serve the app.
+    app.run(host="localhost", port=8080, debug=True)
+
+from google.cloud import datastore, storage
+import time
+
+datastore_client = datastore.Client()
+storage_client = storage.Client()
+
+###
+# Datastore examples
+###
+def list_db_entries():
+    query = datastore_client.query(kind="photos")
+
+    for photo in query.fetch():
+        print(photo.items())
+
+def add_db_entry(object):
+    entity = datastore.Entity(key=datastore_client.key('photos'))
+    entity.update(object)
+
+    datastore_client.put(entity)
+
+
+def fetch_db_entry(object):
+    #print(object)
+
+    query = datastore_client.query(kind='photos')
+
+    for attr in object.keys():
+        query.add_filter(attr, "=", object[attr])
+
+    obj = list(query.fetch())
+
+    #print("fetch")
+    #for photo in obj:
+    #    print(photo.items())
+
+    return obj
+
+###
+# Cloud Storage examples
+###
+def get_list_of_files(bucket_name):
+    """Lists all the blobs in the bucket."""
+    print("\n")
+    print("get_list_of_files: "+bucket_name)
+
+    blobs = storage_client.list_blobs(bucket_name)
+    print(blobs)
+    files = []
+    for blob in blobs:
+        files.append(blob.name)
+
+    return files
+
+def upload_file(bucket_name, file_name):
+    """Send file to bucket."""
+    print("\n")
+    print("upload_file: "+bucket_name+"/"+file_name)
+
+    bucket = storage_client.bucket(bucket_name)
+    blob = bucket.blob(file_name)
+
+    blob.upload_from_filename(file_name)
+
+    return 
+
+def download_file(bucket_name, file_name):
+    """ Retrieve an object from a bucket and saves locally"""  
+    print("\n")
+    print("download_file: "+bucket_name+"/"+file_name)
+    bucket = storage_client.bucket(bucket_name)
+
+    blob = bucket.blob(file_name)
+    blob.download_to_filename(file_name)
+    blob.reload()
+    print(f"Blob: {blob.name}")
+    print(f"Bucket: {blob.bucket.name}")
+    print(f"Storage class: {blob.storage_class}")
+    print(f"Size: {blob.size} bytes")
+    print(f"Content-type: {blob.content_type}")
+    print(f"Public URL: {blob.public_url}")
+
+    return
+
+# print(get_list_of_files("de-andrade-fau"))
+
+# upload_file("de-andrade-fau", "test.txt")
+# print(get_list_of_files("de-andrade-fau"))
+
+# download_file("de-andrade-fau", "test.txt")
+# upload_file("de-andrade-fau", "test1.txt")
+# download_file("de-andrade-fau", "test1.txt")
+# print(get_list_of_files("de-andrade-fau"))
+
+# download_file("de-andrade-fau", "sample_640Ã—426.jpeg")
+
+
+###
+# Datastore
+###
+list_db_entries()
+
+obj = {"name":"fau-rocks.jpeg", "url":"blablabla.com/ricardo.jpeg", "user":"rdeandrade", 'timestamp':int(time.time())}
+add_db_entry(obj)
+
+obj1 = {'user':'rdeandrade'}
+print(obj1)
+result=fetch_db_entry(obj1)
+print(result)
+print(len(result))
